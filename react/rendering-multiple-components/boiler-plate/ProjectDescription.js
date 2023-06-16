f52f00ccf5948298801a1ead1e4dd75a51112898
Rohith10X <122977645+Rohith10x@users.noreply.github.com>
2023-06-16 22:16:21 +0530

    React assignments - Description and boiler plate code (#29)
    * A1 - import export
    
    * A2 : ReactRender Component was updated
    
    * A4: rendering multiple components is updated
    
    * Updated changes as per review
    
    * Typo is fixed
    
    * Changes were made as per suggestions,Please Review
    
    * Avoid return word
    
    ---------
    
    Co-authored-by: Sailesh Ramayanam <33143312+sailesh-ramayanam@users.noreply.github.com>

---
+++ react/rendering-multiple-components/boiler-plate/ProjectDescription.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function ProjectDescription(){
+  return (
+    <div>
+      {/* code for ProjectDescription Component */}
+    </div>
+  )
+}
---
+++ react/rendering-multiple-components/boiler-plate/ProjectName.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function ProjectName(){
+  return (
+    <div>
+      {/* code for ProjectName Component */}
+    </div>
+  )
+}
