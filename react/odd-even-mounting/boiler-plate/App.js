37512fdd8fbe3793b876939da404d32f97c9c845
Avinash10x <avinashsahu1994@gmail.com>
2023-06-20 12:24:12 +0530

    minor changes

--- react/odd-even-mounting/boiler-plate/App.js
+++ react/odd-even-mounting/boiler-plate/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {}
-render();
-return <div id="main"></div>;
-
+class App extends React.Component {
+  render() {
+    return;
+  }
+}
 export default App;
